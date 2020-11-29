99308f03807ea8fb86f8c047bc3c9bbeff6d552b
Yulia Rokotova <fastlagsgatan@gmail.com>
2020-11-29 10:21:38 +0100

    User input part ready.

--- Inlämning1_revisited/Program.cs
+++ Inlämning1_revisited/Program.cs
@@ -6,7 +6,21 @@ namespace Inlämning1_revisited
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter first operator");
+            string operand1 = Console.ReadLine();
+            Console.WriteLine("Enter second operator");
+            string operand2 = Console.ReadLine();
+
+            Console.WriteLine("Enter first term");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second term");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third term");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            Console.WriteLine("====================");
+            Console.WriteLine($"{num1} {operand1} {num2} {operand2} {num3}");
+            Console.WriteLine("====================");
         }
     }
 }
