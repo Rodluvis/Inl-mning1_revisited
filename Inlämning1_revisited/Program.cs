f4af6a74f9767b42849b8c5fa320cbe69aafcb98
Yulia Rokotova <fastlagsgatan@gmail.com>
2020-11-29 10:44:50 +0100

    Logic for succession of calculation is set.

--- Inlämning1_revisited/Program.cs
+++ Inlämning1_revisited/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inlämning1_revisited
 {
@@ -6,43 +7,118 @@ namespace Inlämning1_revisited
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first operator");
-            string operand1 = Console.ReadLine();
-            Console.WriteLine("Enter second operator");
-            string operand2 = Console.ReadLine();
+            Console.Title = "Inlämningsuppgift 1";
+            List<int> resultList = new List<int>();
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Enter first operator");
+                string operand1 = Console.ReadLine();
+                Console.WriteLine("Enter second operator");
+                string operand2 = Console.ReadLine();
 
-            Console.WriteLine("Enter first term");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second term");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter third term");
-            int num3 = Convert.ToInt32(Console.ReadLine());
-            int result = 0;
-            Console.WriteLine("====================");
-            Console.WriteLine($"{num1} {operand1} {num2} {operand2} {num3}");
-            Console.WriteLine("====================");
+                Console.WriteLine("Enter first term");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second term");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter third term");
+                int num3 = Convert.ToInt32(Console.ReadLine());
 
-        }
-            static int Add(int num1, int num2)
-            { 
-            int sum = num1 + num2;
-            return sum;
+                Console.WriteLine("====================");
+                Console.WriteLine($"{num1} {operand1} {num2} {operand2} {num3}");
+                Console.WriteLine("====================");
+                int result = 0;
+
+                if (operand2 == "*" || operand2 == "/")
+                {
+                    switch (operand2)
+                    {
+                        case "*":
+                            num2 = Multiply(num2, num3);
+                            break;
+                        case "/":
+                            num2 = Devide(num2, num3);
+                            break;
+                    }
+                    switch (operand1)
+                    {
+                        case "+":
+                            result = Add(num1, num2);
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                        case "-":
+                            result = Subtract(num1, num2);
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                        case "*":
+                            result = Multiply(num1, num2);
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+
+                        case "/":
+                            result = Devide(num1, num2);
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (operand1)
+                    {
+                        case "+":
+                            num1 += num2;
+                            break;
+                        case "-":
+                            num1 -= num2;
+                            break;
+                        case "*":
+                            num1 *= num2;
+                            break;
+
+                        case "/":
+                            num1 /= num2;
+                            break;
+                    }
+
+
+                    switch (operand2)
+                    {
+                        case "+":
+                            result = Add(num1, num3);
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                        case "-":
+                            result = Subtract(num1, num3);
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                    }
+                    resultList.Add(result);
+
+
+                }
             }
-        static int Subtract(int num1, int num2)
-        {
-            int sum = num1 - num2;
-            return sum;
-        }
-        static int Multiply(int num1, int num2)
-        {
-            int sum = num1 * num2;
-            return sum;
-        }
-        static int Devision(int num1, int num2)
-        {
-            int sum = num1 / num2;
-            return sum;
-        }
 
+                static int Add(int num1, int num2)
+                {
+                    int sum = num1 + num2;
+                    return sum;
+                }
+                static int Subtract(int num1, int num2)
+                {
+                    int sum = num1 - num2;
+                    return sum;
+                }
+                static int Multiply(int num1, int num2)
+                {
+                    int sum = num1 * num2;
+                    return sum;
+                }
+                static int Devide(int num1, int num2)
+                {
+                    int sum = num1 / num2;
+                    return sum;
+                }
+
+            }
+        }
     }
-}
